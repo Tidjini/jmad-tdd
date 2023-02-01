3cbd6ec9400fec290751c12fa49a1b4fbb941e69
tidjini <tidjini.messaoudi@gmail.com>
2023-02-01 22:31:48 +0100

    check solos dynamicaly

--- config/tests.py
+++ config/tests.py
@@ -2,15 +2,35 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+from solos.models import Solo
+
 
 class StudentTestCase(LiveServerTestCase):
 
-    INSTRUMENT_INPUT_ID  = 'jmad-instrument'
-    ARTIST_INPUT_ID  = 'jmad-artist'
-    
+    INSTRUMENT_INPUT_ID = 'jmad-instrument'
+    ARTIST_INPUT_ID = 'jmad-artist'
+
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(5)
+
+        self.solo = Solo.objects.create(
+            track='My Favorite Thing',
+            artist='John Coltrane',
+            instrument='saxophone'
+        )
+
+        self.solo2 = Solo.objects.create(
+            track='All Blues',
+            artist='Canonbal Adderlay',
+            instrument='saxophone'
+        )
+
+        self.solo3 = Solo.objects.create(
+            track='Walts for Debby',
+            artist='Canonbal Adderlay',
+            instrument='saxophone'
+        )
 
     def test_student_find_solos(self):
         """
@@ -21,42 +41,46 @@ class StudentTestCase(LiveServerTestCase):
         brand_element = self.browser.find_element(
             By.CLASS_NAME, 'navbar-brand')
         self.assertEqual('JMAD', brand_element.text)
-        
+
         # He sees the input search form, including labels and placeholder
-        
-        instrument_input = self.browser.find_element(By.CSS_SELECTOR, f'input#{self.INSTRUMENT_INPUT_ID}')
-        self.assertIsNotNone(self.browser.find_element(By.CSS_SELECTOR, f'label[for="jmad-instrument"]'))
-        self.assertEqual(instrument_input.get_attribute('placeholder'), 'i.e. trumpet')
-        
-        
-        
-        artist_input = self.browser.find_element(By.CSS_SELECTOR, f'input#{self.ARTIST_INPUT_ID}')
-        self.assertIsNotNone(self.browser.find_element(By.CSS_SELECTOR, f'label[for="jmad-artist"]'))
-        self.assertEqual(artist_input.get_attribute('placeholder'), 'i.e. davis')
+
+        instrument_input = self.browser.find_element(
+            By.CSS_SELECTOR, f'input#{self.INSTRUMENT_INPUT_ID}')
+        self.assertIsNotNone(self.browser.find_element(
+            By.CSS_SELECTOR, f'label[for="jmad-instrument"]'))
+        self.assertEqual(instrument_input.get_attribute(
+            'placeholder'), 'i.e. trumpet')
+
+        artist_input = self.browser.find_element(
+            By.CSS_SELECTOR, f'input#{self.ARTIST_INPUT_ID}')
+        self.assertIsNotNone(self.browser.find_element(
+            By.CSS_SELECTOR, f'label[for="jmad-artist"]'))
+        self.assertEqual(artist_input.get_attribute(
+            'placeholder'), 'i.e. davis')
         # handling the inputs typing
-        
-        # handling the inputs typing 
+
+        # handling the inputs typing
         instrument_input.send_keys('saxophone')
-        instrument_input.submit()
-        # self.browser.find_element(By.CSS_SELECTOR, 'form button').click()
-                
+        # instrument_input.submit()
+
         # he sees many results, so he adds a particular
         # artist to his search query
-        searched_results = self.browser.find_elements(By.CLASS_NAME, 'jmad-search-result')
+        searched_results = self.browser.find_elements(
+            By.CLASS_NAME, 'jmad-search-result')
         self.assertGreater(len(searched_results), 2)
-        
+
         # ...so he adds a particular artist to his search query, and gets
         # more manageble list
-        second_artist_input = self.browser.find_element(By.CSS_SELECTOR, f'input#{self.ARTIST_INPUT_ID}')
-        second_artist_input.send_keys('Cannonbal Adderley')
-        second_artist_input.submit()
-        
-        second_searched_results = self.browser.find_elements(By.CLASS_NAME, 'jmad-search-result')
+        second_artist_input = self.browser.find_element(
+            By.CSS_SELECTOR, f'input#{self.ARTIST_INPUT_ID}')
+        second_artist_input.send_keys('Canonbal Adderlay')
+        # second_artist_input.submit()
+        self.browser.find_element(By.CSS_SELECTOR, 'form button').click()
+
+        second_searched_results = self.browser.find_elements(
+            By.CLASS_NAME, 'jmad-search-result')
         self.assertEqual(len(second_searched_results), 2)
 
-         
-        
-        
         self.fail('Incomplete Test')
 
     def tearDown(self):
--- solos/views.py
+++ solos/views.py
@@ -4,9 +4,17 @@ from .models import Solo
 
 
 def index(request):
-    context = {
-        'solos': Solo.objects.filter(
+    solos_queryset = Solo.objects.all()
+
+    if request.GET.get('instrument', None):
+        solos_queryset = solos_queryset.filter(
             instrument=request.GET.get('instrument', None)
         )
-    }
+
+    if request.GET.get('artist', None):
+        solos_queryset = solos_queryset.filter(
+            artist=request.GET.get('artist', None)
+        )
+
+    context = {'solos': solos_queryset}
     return render(request, 'solos/index.html', context)

67b23e2df29a12b9bbe3fa43b36c8f0dbfe32374
tidjini <tidjini.messaoudi@gmail.com>
2023-02-09 18:46:34 +0100

    Adds Default Functionnality for SoloDetailView

--- solos/tests/test_views.py
+++ solos/tests/test_views.py
@@ -53,9 +53,9 @@ class SoloViewTestCase(SoloBaseTestCase):
     def test_basic(self):
         '''check page response is 200, and usees the correct template, and has the correct context'''
         request = self.factory.get('/solo/1/')
-        response = SoloDetailView.as_view()(request, self.drum_solo.pk)
+        response = SoloDetailView.as_view()(request, pk=self.drum_solo.pk)
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.contex_data['solo'].artis, 'Rich')
+        self.assertEqual(response.context_data['solo'].artist, 'Rich')
         with self.assertTemplateUsed('solos/solo_detail.html'):
             response.render()

a00dfb26ad67464684ff16c2be97651f3b332e28
tidjini <tidjini.messaoudi@gmail.com>
2023-02-17 11:30:47 +0100

    Order Tracks and Albums

--- albums/models.py
+++ albums/models.py
@@ -8,15 +8,23 @@ class Album(models.Model):
     artist = models.CharField(max_length=100)
     slug = models.SlugField()
 
+    class Meta:
+        ordering = ['name']
+
     def __str__(self):
         return self.name
 
+
+# ordering with foreing keys -> must order the origin Album with tracks
 
 class Track(models.Model):
     name = models.CharField(max_length=100)
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     track_number = models.PositiveIntegerField(blank=True, null=True)
     slug = models.SlugField()
+
+    class Meta:
+        ordering = ['album', 'track_number']
 
     def __str__(self) -> str:
         return self.name

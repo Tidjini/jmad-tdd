b8f527baa2504d1a2b409717dc93d5099ddc8092
tidjini <tidjini.messaoudi@gmail.com>
2023-02-20 19:17:06 +0100

    Slugify artist name in Serializer Validation

--- solos/serializers.py
+++ solos/serializers.py
@@ -1,9 +1,15 @@
+from django.utils.text import slugify
 from rest_framework import serializers
 
 from .models import Solo
 
 
 class SoloSerializer(serializers.HyperlinkedModelSerializer):
+
+    def validate(self, data):
+        data['slug'] = slugify(data['artist'])
+        return data
+
     class Meta:
         model = Solo
         fields = '__all__'

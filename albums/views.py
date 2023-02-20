5c6dddbe3c9dd5822bb4f47f2d1cb3c677e07843
tidjini <tidjini.messaoudi@gmail.com>
2023-02-20 18:16:08 +0100

    set Album view serializer

--- albums/views.py
+++ albums/views.py
@@ -1,8 +1,10 @@
 from rest_framework import viewsets, mixins
 
 from .models import Album
+from .serializers import AlbumSerializer
 # Create your views here.
 
 
 class AlbumViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     queryset = Album.objects.all()
+    serializer_class = AlbumSerializer

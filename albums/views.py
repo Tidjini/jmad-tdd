09983f015de99b1ef7f90600a430780611901907
tidjini <tidjini.messaoudi@gmail.com>
2023-02-20 18:09:17 +0100

    set Album view set

--- albums/views.py
+++ albums/views.py
@@ -1,6 +1,8 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 
 from .models import Album
 # Create your views here.
-class AlbumViewSet(viewsets.ViewSet):
+
+
+class AlbumViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     queryset = Album.objects.all()
